e4d9a93ac0fb7c32e71f7c16325045bfbe6e14a7
jramirezgomez1 <jramirezgomez1@toromail.csudh.edu>
2026-02-25 02:07:35 +0000

    lab 7

---
+++ jramirez7.py
@@ -0,0 +1,27 @@
+#Juan 
+#lab #7
+
+
+x = 5
+y = "Pickle"
+z = 8.8
+
+if x > 6:
+    print("X is greater than 6")
+elif x < 6:
+    print("X is less than 6 and the value of x is ",x)
+else:
+    print("x is ",x," which is equal to 6")
+
+
+if y == "Pickle":
+    print("This varialbe is a pickle")
+else:
+    print("Is not a pickle on the variable")
+
+if z > 9:
+    print("z is greater than 9")
+elif x < 9:
+    print("z is less than 9 and z is ",z)
+else:
+    print("z is",z," which is equal to 9")
