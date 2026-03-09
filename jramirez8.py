db103342ef4eead9789770afcfcc8f83d80fae95
jramirezgomez1 <jramirezgomez1@toromail.csudh.edu>
2026-03-09 02:46:56 +0000

    Lab 8

---
+++ jramirez8.py
@@ -0,0 +1,21 @@
+#Juan Ramirez
+
+userinput  = input("Enter a number of 0 - 9: ")
+a = int(userinput)
+b = 6
+if (a > b):
+     print("Sorry you guess a high number")
+elif (a < b):
+     print("Sorry you guess a low number")
+else:
+     print("CONGRATS you guess right!")
+
+
+for i in range(5):
+    print("What kind of computer sings the best?.... A Dell")
+
+x = 4
+
+while(x > 0):
+    print("Pepperoni Pizza")
+    x = x - 1
