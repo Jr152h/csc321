54d573833955e74fea661638569612ac4836c1cb
jramirezgomez1 <jramirezgomez1@toromail.csudh.edu>
2026-04-24 01:41:43 +0000

    midterm2

---
+++ midterm2.c
@@ -0,0 +1,35 @@
+//Juan Ramirez
+
+#include <stdio.h>
+#include <stdlib.h>
+
+		
+int name(int x);
+
+
+stdin main(void)
+{
+	int a = 0;
+	printf("Enter a number: ");
+	scanf("%d",&a);
+
+
+
+	printf(name(a));
+
+	return EXIT_SUCCESS;
+
+}
+
+int name(int x)
+{
+
+	for(int i = 0;i < 4; i++)
+	{
+		printf("Juan",&x)
+
+	}
+
+}
+
+
